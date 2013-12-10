2be48f8645d45abd41322f26a92de7671361f329
Tiago Cruzeiro <tiago.c.1991@gmail.com.pt>
2013-12-10 17:07:26 +0000

    Initial files

---
+++ tiles.pl
@@ -0,0 +1,5 @@
+
+%tile(triangle1color, triangle2color, triangle3color, triangle4color, triangle5color, triangle6color, triangle7color, triangle8color). 
+%
+% | 1 \ 2 | 3 / 4 |
+% | 5 / 6 | 7 \ 8 |
